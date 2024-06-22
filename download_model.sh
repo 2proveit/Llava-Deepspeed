1c94852b946e7cac2b5915b260cd3cfdfcdf5776
xinlongli <xinlongli@deepglint.com>
2024-06-22 22:27:32 +0800

    add dataset download

--- download_model.sh
+++ download_model.sh
@@ -1,6 +1,5 @@
 export HF_ENDPOINT=https://hf-mirror.com
 
-huggingface-cli download --resume-download openai/clip-vit-large-patch14-336 --local-dir openai/clip-vit-large-patch14-336 --local-dir-use-symlinks False
-
-
+huggingface-cli download --resume-download liuhaotian/LLaVA-CC3M-Pretrain-595K --local-dir LLaVA-CC3M-Pretrain-595K --local-dir-use-symlinks False
+huggingface-cli download --resume-download openai/clip-vit-large-patch14-336 --local-dir openai-clip-vit-large-patch14-336 --local-dir-use-symlinks False
 huggingface-cli download --resume-download Qwen/Qwen1.5-4B-Chat --local-dir Qwen1.5-4B-Chat --local-dir-use-symlinks False
