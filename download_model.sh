7ea9ac5a3389447275ee1d497edb4f917e8c7695
xinlongli <xinlongli@deepglint.com>
2024-06-22 21:31:14 +0800

    init download model script]

---
+++ download_model.sh
@@ -0,0 +1,6 @@
+export HF_ENDPOINT=https://hf-mirror.com
+
+huggingface-cli download --resume-download openai/clip-vit-large-patch14-336 --local-dir openai/clip-vit-large-patch14-336 --local-dir-use-symlinks False
+
+
+huggingface-cli download --resume-download Qwen/Qwen1.5-4B-Chat --local-dir Qwen1.5-4B-Chat --local-dir-use-symlinks False
