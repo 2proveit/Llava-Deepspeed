1bacc89a539d22e90bc6cfc9004eca30864b2e85
xinlongli <lxl2067956020@gmail.com>
2024-06-23 16:08:31 +0800

    fix some bugs for load data

--- data.py
+++ data.py
@@ -24,13 +24,13 @@ class QAImageOutput:
 class LlavaDataset(Dataset):
     def __init__(self, data_args:DatasetArgs) -> None:
         super(LlavaDataset).__init__()
-        self.chat_data, self.img_dir = self.build_dataset(data_args.dataset_path)
+        self.chat_data, self.image_dir = self.build_dataset(data_args.dataset_path)
         
     def build_dataset(self, data_dir: str) -> Tuple[List[Dict], Path]:
         data_dir = Path(data_dir)
-        assert os.path.exsits(data_dir)
+        assert os.path.exists(data_dir)
         chat_file = data_dir.joinpath("chat.json")
-        image_dir = data_dir.joinpath("images_dl")
+        image_dir = data_dir.joinpath("images")
 
         chat_data = pd.read_json(chat_file).to_dict(orient="records")
 
@@ -43,7 +43,7 @@ class LlavaDataset(Dataset):
         cur_data = self.chat_data[idx]
         conversations = cur_data.get('conversations')
         human_input = conversations[0].get('value')
-        chatbot_output = conversations[0].get('value')
+        chatbot_output = conversations[1].get('value')
         image_path = self.image_dir.joinpath(cur_data.get('image'))
         return human_input, chatbot_output, image_path
     
@@ -72,20 +72,21 @@ def build_q_a_img(processor:AutoProcessor, q_text:str, a_text:str, img_path:str)
     return res
 
 class LlavaDataCollector:
-    def __init__(self, processor:AutoProcessor, max_length:int, IGNORE_ID:int=-100,):
+    def __init__(self, processor:AutoProcessor, IGNORE_ID:int=-100,):
         self.eos_id = processor.tokenizer.eos_token_id
         self.pad_id = processor.tokenizer.pad_token_id if processor.tokenizer.pad_token_id is not None else processor.tokenizer.eos_token_id
-        self.max_length = max_length
+        self.processor = processor
         self.ignore_id = IGNORE_ID
     
     def convert_one(self, q_input_ids:torch.Tensor, a_input_ids:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        input_ids = torch.cat([q_input_ids, a_input_ids, self.eos_id], dim=-1)
+        eos_tensor = torch.tensor([self.eos_id]).unsqueeze(0)
+        input_ids = torch.cat([q_input_ids, a_input_ids, eos_tensor], dim=-1)
         labels = torch.cat(
-            (torch.full((q_input_ids.size(0),), self.ignore_id, dtype=torch.long),
-            a_input_ids, self.eos_id), dim=-1)
+            [torch.full((1, q_input_ids.size(0)), self.ignore_id, dtype=torch.long),
+            a_input_ids, eos_tensor], dim=-1)
         return input_ids, labels
     
-    def __call__(self, batch:List[Any]) -> Dict[str: torch.Tensor]:
+    def __call__(self, batch:List[Any]) -> Dict[str, torch.Tensor]:
         input_id_list = []
         labels_list = []
         pixel_values_list = []
@@ -93,26 +94,28 @@ class LlavaDataCollector:
         for batch_item in batch:
             qa_img_output = build_q_a_img(
                 self.processor,
-                **batch_item
+                *batch_item
             )
             
             input_ids, labels = self.convert_one(
                 qa_img_output.q_input_ids,
                 qa_img_output.a_input_ids
             )
-            if input_ids.size(0) > max_length:
-                max_length = input_ids.size(0)
+            if input_ids.size(1) > max_length:
+                max_length = input_ids.size(1)
 
             input_id_list.append(input_ids)
             labels_list.append(labels)
-            pixel_values_list.append(qa_img_output.pixel_values)     
-
+            pixel_values_list.append(qa_img_output.pixel_values)
+            
         # left padding
-        input_ids = torch.stack(
-            [torch.cat([torch.full((max_length - ids.size(0),), self.pad_id, dtype=torch.long), ids]) for ids in input_id_list]
+        input_ids = torch.cat(
+            [torch.cat([torch.full((1, max_length - ids.size(1)), self.pad_id, dtype=torch.long), ids], dim=-1) for ids in input_id_list],
+            dim=0
         )
-        labels = torch.stack(
-            [torch.cat([torch.full((max_length - ids.size(0),), self.ignore_id, dtype=torch.long), ids]) for ids in labels_list]
+        labels = torch.cat(
+            [torch.cat([torch.full((1, max_length - ids.size(1),), self.ignore_id, dtype=torch.long), ids], dim=-1) for ids in labels_list],
+            dim=0
         )
         pixel_values = torch.cat(pixel_values_list, dim=0)
         attention_mask = torch.ones_like(input_ids)
